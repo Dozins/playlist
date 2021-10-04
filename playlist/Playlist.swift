ccd288ffa95838b6182fa34fc724658125eae34a
Jayden <ciebiesaraningo@gmail.com>
2021-10-04 16:53:20 +0800

    Added stuff.

---
+++ playlist/Playlist.swift
@@ -0,0 +1,12 @@
+//
+//  Playlist.swift
+//  playlist
+//
+//  Created by Jayden Lee on 4/10/21.
+//
+
+import Foundation
+
+struct Playlist {
+    
+}
---
+++ playlist/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  playlist
+//
+//  Created by Jayden Lee on 4/10/21.
+//
+
+import Foundation
+
+struct Song {
+    var title: String
+    var artist: String
+}
