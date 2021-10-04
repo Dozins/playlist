e9731b22c057da3d4a8bbde879e7946cfcec9c35
Jayden <ciebiesaraningo@gmail.com>
2021-10-04 16:42:32 +0800

    Initial Commit

---
+++ playlist/playlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  playlistApp.swift
+//  playlist
+//
+//  Created by Jayden Lee on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct playlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
